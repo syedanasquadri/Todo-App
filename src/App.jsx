203093da99441fa902f6e62bb5866f22825d525f
syedanasquadri <syedanasquadri123@gmail.com>
2025-09-27 16:50:07 +0530

    Added Project files

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+const App = () => {
+  const[Todos, setTodos] = useState([])
+  const [inputVal, setinputVal] = useState("");
+  function handleChange(e){
+   setinputVal(e.target.value)
+  }
+  function AddTodo(){
+    if(!inputVal.trim()){
+      console.log("first add something")
+    };
+    setTodos([...Todos,inputVal]);
+    setinputVal("");
+  }
+  // function DeleteTodo(index){
+  //   const newArr=[Todos.filter((Todos) => Todos=!Todos.index)]
+  //   setTodos([newArr])
+  // }
+  function DeleteTodo(index){
+    setTodos(Todos.filter((_,i) => i!==index))
+  }
+
+  return <div>
+    <h1>Todo</h1>
+    <input
+     type="text"
+     onChange={handleChange}
+     value={inputVal}
+    />
+    <button onClick={AddTodo}>Add</button>
+    <ul>
+      {Todos.map((x,index) => 
+      <li key={index}>{x} <button key={index} onClick={() => DeleteTodo(index)}>Delete</button></li>)}  
+    </ul>
+  </div>
+}
+
+export default App
